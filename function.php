42609d6eb3342146f05e2b2c05b87ad1e247ce40
sonindra <92632445+sonindra@users.noreply.github.com>
2023-03-22 23:58:42 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function sayhi($name, $age ){
+            echo "Hello $name,you are $age <br>";
+
+
+        //echo "Hello user";
+
+    }
+    
+    sayhi("SONINDRA",35);
+// funtion are reusable set of code 
+        sayhi("MAHI",25);
+
+        sayhi("RAVI",17);
+    ?>
+</body>
+</html>
